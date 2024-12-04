528edf39b55b6cb900ac497af7e70859dcabac97
Hichem Fantar <fantar.hichem@gmail.com>
2024-12-04 12:52:14 +0100

    Update ESLint and Prettier dependencies across packages

--- packages/bluesky-embed-core/.eslintrc.cjs
+++ packages/bluesky-embed-core/.eslintrc.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
 };
